56f0f18e9f00e41b04f4af3196be50e2e8992abd
unknown <itsdy2@gmail.com>
2026-01-29 00:27:28 +0900

    Refactor setup.py to match working reference

--- setup.py
+++ setup.py
@@ -1,21 +1,49 @@
 # -*- coding: utf-8 -*-
+import traceback
 from plugin import *
-from .constants import OPTS
 
-# Current file path setup
-OPTS['filepath'] = __file__
+# 1. Plugin Setup
+setting = {
+    'filepath': __file__,
+    'use_db': True,
+    'use_default_setting': True,
+    'home_module': 'base', 
+    'menu': {
+        'uri': __package__,
+        'name': '주식 투자 참고',
+        'list': [
+            {
+                'uri': 'base',
+                'name': '설정',
+                'list': [
+                    {'uri': 'setting', 'name': '기본 설정'},
+                ]
+            },
+            {
+                'uri': 'analysis',
+                'name': '분석',
+                'list': [
+                    {'uri': 'dashboard', 'name': '대시보드'},
+                    {'uri': 'list', 'name': 'ETF 상대강도'},
+                ]
+            }
+        ]
+    },
+    'default_route': 'normal',
+}
 
-# Plugin Initialization
-P = create_plugin_instance(OPTS)
-PLUGIN = P
+# 2. Create Plugin Instance
+P = create_plugin_instance(setting)
 
 try:
+    # 3. Define ModelSetting
+    PluginModelSetting = P.ModelSetting
+
+    # 4. Import and Register Modules
     from .presenters import ModuleBase, ModuleAnalysis
     
     P.set_module_list([ModuleBase, ModuleAnalysis])
     
-    PluginModelSetting = P.ModelSetting
-
 except Exception as e:
     P.logger.error(f'Exception:{str(e)}')
     P.logger.error(traceback.format_exc())
